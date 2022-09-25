574d01cebf04e35ef77dde6baba356c0d6c1547f
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-09-25 20:26:40 +0530

    Added: checked employee present or absent

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,1 +1,8 @@
 console.log("Welcome to employee wage computation program");
+const present=1;
+let isPresent=Math.floor(Math.random()*2);
+console.log(isPresent);
+if(isPresent==present)
+console.log("Employee is present");
+else
+console.log("Employee is absent");
